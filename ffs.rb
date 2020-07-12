f6bd786eac4f9a96e700fc67dcf853352093cb5f
Anthony Risch <rischthefisch@gmail.com>
2020-07-12 09:15:08 +0000

    Automatically backed up by Learn

---
+++ ffs.rb
@@ -0,0 +1,18 @@
+def greeting_programmer(name, language)
+  puts "hello, #{name}. We heard you are a great #{language} prgrammer."
+end
+greeting_programmer("MJ", "ruby")
+
+def say_hi
+  puts "hi!"
+end
+
+say_hi
+
+describe "MyRubyThing" do
+  it "runs" do  
+  end
+end
+
+
+!!"hello"
